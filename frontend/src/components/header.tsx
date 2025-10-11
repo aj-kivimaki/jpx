72a18c2b60db6ba56ca7d1e81320b36ee77e0614
aj-kivimaki <aj.kivimaki@gmail.com>
2025-10-11 08:12:21 +0300

    reset css, style header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -10,35 +10,33 @@ const Header = () => {
 
   return (
     <header className="header">
-      <h1 className="title">J. Partynen</h1>
-      <div className="nav">
-        {/* Desktop Menu */}
+      <div>
+        <h1 className="title">J. Partynen</h1>
+      </div>
+      <div>
         <nav className="desktop-menu">
           <a href="#gigs">Keikat</a>
           <a href="#contact">Yhteys</a>
           <a href="#promo">Promo</a>
+          <a href="#news">Uutiset</a>
         </nav>
-
-        {/* Mobile Menu Button */}
-        <div className="mobile-menu-button" onClick={toggleMenu}>
-          {isOpen ? '✖' : '☰'}
-        </div>
-
-        {/* Mobile Menu */}
-        {isOpen && (
-          <nav className="mobile-menu">
-            <a href="#gigs" onClick={toggleMenu}>
-              Keikat
-            </a>
-            <a href="#contact" onClick={toggleMenu}>
-              Yhteys
-            </a>
-            <a href="#promo" onClick={toggleMenu}>
-              Promo
-            </a>
-          </nav>
-        )}
       </div>
+      <div className="mobile-menu-button" onClick={toggleMenu}>
+        {isOpen ? '✖' : '☰'}
+      </div>
+      {isOpen && (
+        <nav className="mobile-menu">
+          <a href="#gigs" onClick={toggleMenu}>
+            Keikat
+          </a>
+          <a href="#contact" onClick={toggleMenu}>
+            Yhteys
+          </a>
+          <a href="#promo" onClick={toggleMenu}>
+            Promo
+          </a>
+        </nav>
+      )}
     </header>
   );
 };
