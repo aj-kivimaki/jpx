5fd7012d2085c3e2316aaf0e0036ec087a85aa2b
aj-kivimaki <aj.kivimaki@gmail.com>
2025-10-12 12:27:47 +0300

    backend cors settings

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,12 @@ import gigsRouter from "./routes/gigs.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/gigs", gigsRouter);
@@ -14,4 +19,6 @@ app.use("/api/gigs", gigsRouter);
 app.get("/", (req, res) => res.send("Band API is live!"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
