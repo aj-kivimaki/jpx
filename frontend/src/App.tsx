cebb946159464a0c6fae6083638cd2b1c3cb73ee
aj-kivimaki <aj.kivimaki@gmail.com>
2025-10-25 13:10:36 +0300

    add favicon, change styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,9 @@ import Banner from './components/Banner';
 import Gigs from './components/Gigs';
 import Info from './components/Info';
 import Footer from './components/Footer';
-
+import './components/styles/reset.css';
+import './components/styles/global.css';
+import './components/styles/App.css';
 
 const App = () => {
   return (
@@ -18,6 +20,6 @@ const App = () => {
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
