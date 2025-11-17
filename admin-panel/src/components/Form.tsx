053369cac82582ad0afd6fe9505225f0910bf5d7
aj-kivimaki <aj.kivimaki@gmail.com>
2025-11-17 18:25:05 +0200

    add ci/cd for admin

--- admin-panel/src/components/Form.tsx
+++ admin-panel/src/components/Form.tsx
@@ -1,7 +1,7 @@
 import styles from './Form.module.css';
 
 const Form = () => {
-  return <div className={styles.form}>Form Component</div>;
+  return <div className={styles.form}>Form</div>;
 };
 
 export default Form;
