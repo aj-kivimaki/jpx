092cad15b71917c69038c2cde836363ee4833fcb
aj-kivimaki <aj.kivimaki@gmail.com>
2025-11-24 22:17:41 +0200

    fix path error

--- packages/admin-panel/vite.config.ts
+++ packages/admin-panel/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Map 'shared' to the src folder of the shared package
+      shared: path.resolve(__dirname, '../shared/src'),
+    },
+  },
 });
