624d66559cf19f56db811b2d28c30f2babb3bbad
Atte Kivimäki <121675713+aj-kivimaki@users.noreply.github.com>
2025-11-26 21:05:45 +0200

    Refactor/extract data - separate data and config files (#19)
    * separate data and config files
    
    * fix import paths

--- packages/frontend/src/hooks/useScrolling.ts
+++ packages/frontend/src/hooks/useScrolling.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { scrollConfig } from 'shared/src/config/scroll';
+import { scrollConfig } from 'shared/config/scroll';
 
 const { threshold, label } = scrollConfig;
 
