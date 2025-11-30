39599287c7417a80ad7d1cc58899628e51a3f5db
Atte Kivimäki <121675713+aj-kivimaki@users.noreply.github.com>
2025-11-30 22:46:23 +0200

    use dialog tag for modal (#50)

--- packages/frontend/src/components/sidebar/Sidebar.tsx
+++ packages/frontend/src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,30 @@
-import { lazy, useState } from 'react';
+import { useRef } from 'react';
+import { site, social } from 'shared';
 import { FaInstagram, FaFacebook, FaYoutube, FaSpotify } from 'react-icons/fa';
 import { CiSettings } from 'react-icons/ci';
-import { social } from 'shared';
-import styles from './Sidebar.module.css';
 import { FaCartShopping } from 'react-icons/fa6';
+import { IoMdCloseCircleOutline } from 'react-icons/io';
+import styles from './Sidebar.module.css';
+import LanguageSwitcher from '../LanguageSwitcher';
+import useLocalized from '../../hooks/useLocalized';
+import ModeSwitcher from '../ModeSwitcher';
 
-const SettingsModal = lazy(() => import('../modal/SettingsModal'));
+// const SettingsModal = lazy(() => import('../modal/SettingsModal'));
 
 const Sidebar = () => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+  const localize = useLocalized();
+
+  const modalSection = site.sections.find((s) => s.id === 'modal');
+
+  const openModal = () => {
+    dialogRef.current?.showModal();
+  };
+
+  const closeModal = () => {
+    dialogRef.current?.close();
+  };
+
   return (
     <div className={styles.sidebar}>
       {social.map((link) => (
@@ -30,12 +46,30 @@ const Sidebar = () => {
       <button
         className={styles.options}
         aria-label="Options"
-        onClick={() => setIsModalOpen(true)}
+        onClick={openModal}
       >
         <CiSettings />
       </button>
       {/* Modal */}
-      <SettingsModal open={isModalOpen} onClose={() => setIsModalOpen(false)} />
+      <dialog ref={dialogRef} closedby="any">
+        <h2 className={styles.title}>{localize(modalSection?.title)}</h2>
+
+        <div className={styles.modalContent}>
+          <div className={styles.section}>
+            <h2 className={styles.label}>
+              {localize(modalSection?.theme.label)}
+            </h2>
+            <ModeSwitcher />
+          </div>
+          <div className={styles.section}>
+            <h2 className={styles.label}>{localize(modalSection?.language)}</h2>
+            <LanguageSwitcher />
+          </div>
+        </div>
+        <button className={styles.closeButton} onClick={closeModal}>
+          <IoMdCloseCircleOutline />
+        </button>
+      </dialog>
     </div>
   );
 };
