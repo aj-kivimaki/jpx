b140dc6b88a0a80fe6ff4dd5a6c648a5056554c7
aj-kivimaki <aj.kivimaki@gmail.com>
2025-11-30 22:44:09 +0200

    use dialog tag for modal

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,19 +1,13 @@
-import { lazy, Suspense } from 'react';
 import SocialSidebar from './components/sidebar/Sidebar';
 import Header from './components/layout/Header';
 import Banner from './components/layout/Banner';
 import Gigs from './components/gigs/Gigs';
 import Info from './components/info/Info';
 import Footer from './components/layout/Footer';
-import { useModalStore } from '../src/store/modalStore';
 import 'shared/styles/reset.css';
 import 'shared/styles/global.css';
 
-const SettingsModal = lazy(() => import('./components/modal/SettingsModal'));
-
 const App = () => {
-  const { isSettingsOpen, closeSettings } = useModalStore();
-
   return (
     <div>
       <Header />
@@ -22,10 +16,6 @@ const App = () => {
       <Info />
       <Footer />
       <SocialSidebar />
-
-      <Suspense fallback={<div>Loading modal...</div>}>
-        <SettingsModal open={isSettingsOpen} onClose={closeSettings} />
-      </Suspense>
     </div>
   );
 };
