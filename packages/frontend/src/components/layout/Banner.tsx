dc1755adbc425f2a110b68fb37449a02355dc986
Atte Kivimäki <121675713+aj-kivimaki@users.noreply.github.com>
2025-12-01 11:29:53 +0200

    portrait img w/ picture tag (#51)

--- packages/frontend/src/components/layout/Banner.tsx
+++ packages/frontend/src/components/layout/Banner.tsx
@@ -10,12 +10,21 @@ const Banner: React.FC = () => {
   const bannerSection = sections.find((s) => s.id === 'banner');
   const adjectives = localizeArray(bannerSection?.adjectives);
   const soloImage = images.find((img) => img.id === 'solo');
+  const soloPortraitImage = images.find((img) => img.id === 'solo-portrait');
+
   const src = soloImage?.src ?? '';
+  const soloPortraitSrc = soloPortraitImage?.src ?? '';
   const alt = localize(soloImage?.alt);
 
   return (
     <div id={sectionIds.top} className={styles.banner}>
-      <img src={src} alt={alt} className={styles.bannerImg} />
+      <picture>
+        <source
+          srcSet={soloPortraitSrc}
+          media="(orientation: portrait) and (max-width: 560px)"
+        />
+        <img src={src} alt={alt} className={styles.bannerImg} />
+      </picture>
       <div className={styles.overlay}>
         <div className={styles.adjectives}>
           {adjectives.map((adj: string) => (
