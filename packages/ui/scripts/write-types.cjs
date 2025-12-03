a35b7f7e52534368fb8d729e3157ce7a449785e8
aj-kivimaki <aj.kivimaki@gmail.com>
2025-12-03 23:27:50 +0200

    fix: sonarcloud

--- packages/ui/scripts/write-types.cjs
+++ packages/ui/scripts/write-types.cjs
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('deno:path');
 
 const out = path.join(__dirname, '..', 'dist', 'types', 'index.d.ts');
 fs.writeFileSync(out, 'export * from "./src/index";');
