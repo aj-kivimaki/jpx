65bbab7bf85edb776bc043f136d1d262d1788369
aj-kivimaki <aj.kivimaki@gmail.com>
2025-12-10 21:46:46 +0200

    fix: *.d.ts

--- packages/ui/src/declarations.d.ts
+++ packages/ui/src/declarations.d.ts
@@ -3,15 +3,18 @@
 
 // JSON module typings
 declare module '*.json' {
-  const value: any;
+  const value: unknown;
   export default value;
 }
 
 declare global {
   namespace JSX {
-    interface Element extends React.ReactElement<any, any> {}
+    interface Element extends React.ReactElement {}
     interface IntrinsicElements {
-      [elemName: string]: any;
+      [elemName: string]: React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
     }
   }
 }
