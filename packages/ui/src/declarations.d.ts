de7d350b6d7164e1ef455623fae6d54fb04b72c6
aj-kivimaki <aj.kivimaki@gmail.com>
2025-12-10 21:31:32 +0200

    fix: *.d.ts

--- packages/ui/src/declarations.d.ts
+++ packages/ui/src/declarations.d.ts
@@ -1,22 +1,19 @@
 /// <reference types="react" />
 /// <reference types="react-dom" />
 
-// CSS/SCSS module typings
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
 // JSON module typings
 declare module '*.json' {
   const value: any;
   export default value;
 }
 
-// Minimal global JSX namespace (for libraries like react-icons)
-declare namespace JSX {
-  interface Element extends React.ReactElement<any, any> {}
-  interface IntrinsicElements {
-    [elemName: string]: any;
+declare global {
+  namespace JSX {
+    interface Element extends React.ReactElement<any, any> {}
+    interface IntrinsicElements {
+      [elemName: string]: any;
+    }
   }
 }
+
+export {};
