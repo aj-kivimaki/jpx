81ddcec9e9a4ddd5b5d973bf7cad91529ac621f1
aj-kivimaki <aj.kivimaki@gmail.com>
2025-12-11 15:39:38 +0200

    refactor: move utils to /shared, parse envs in apps

--- apps/frontend/src/config/env.ts
+++ apps/frontend/src/config/env.ts
@@ -1,5 +1,4 @@
-import { EnvSchema, rawEnv } from '@jpx/shared';
-import { errorIfMissing, parseRequired } from '@jpx/shared';
+import { EnvSchema, errorIfMissing, parseRequired, rawEnv } from '@jpx/shared';
 
 export const parsedEnv = errorIfMissing(
   parseRequired(EnvSchema, rawEnv, 'Environment'),
